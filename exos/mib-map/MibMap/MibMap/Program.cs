076872489e7d50da31f1e1462016cff318ee76a9
JerryCleuet <150446786+JerryCleuet@users.noreply.github.com>
2025-09-10 09:29:10 +0200

    Livrable 1

---
+++ exos/mib-map/MibMap/MibMap/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MibMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i18n = new Dictionary<string, string>()
+            {
+                { "Pommes","Apples"},
+                { "Poires","Pears"},
+                { "Pastèques","Watermelons"},
+                { "Melons","Melons"},
+                { "Noix","Nuts"},
+                { "Raisin","Grapes"},
+                { "Pruneaux","Plums"},
+                { "Myrtilles","Blueberries"},
+                { "Groseilles","Berries"},
+                { "Tomates","Tomatoes"},
+                { "Courges","Pumpkins"},
+                { "Pêches","Peaches"},
+                { "Haricots","Beans"}
+            };
+
+            List<Product> products = new List<Product>
+            {
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Pastèques", Quantity = 14,Unit = "pièce", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Melons", Quantity = 5,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
+
+                //...
+            };
+
+            var changedProducer = products.Select(p => $"{p.Producer.Substring(0, 3)}...{p.Producer.Substring(p.Producer.Length -1)} {p.ProductName = i18n[p.ProductName]} {p.Quantity * p.PricePerUnit}");
+            Console.WriteLine("Seller | Product | CA");
+            changedProducer.ToList().ForEach(name => { Console.WriteLine(name); });
+
+
+
+
+        }
+
+        class Product
+        {
+            public int Location  { get; set; }
+            public string Producer { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public string Unit { get; set; }
+            public double PricePerUnit { get; set; }
+        }
+    }
+}
